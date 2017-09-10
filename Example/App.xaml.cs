003c303e8d1ea2e0588f169e1a38bf1682e32301
735579768@qq.com <735579768@qq.com>
2017-09-10 18:07:26 +0800

    add

---
+++ Example/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Example
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            Ezhu.AutoUpdater.Updater.CheckUpdateStatus();
+        }
+        
+    }
+}
